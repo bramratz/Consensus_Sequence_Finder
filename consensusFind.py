55c14359346593df98315f1e302d47ebec439afe
Bram Ratz <bratz@uoguelph.ca>
2020-06-15 14:21:05 -0400

    File handling Added file handling loop. Converted list of lists to numpy array for easy future work.

--- consensusFind.py
+++ consensusFind.py
@@ -8,11 +8,24 @@ Created on Mon Jun 15 12:52:55 2020
 
 # Import modules 
 import sys
+import numpy as np
 
+# List to hold lines from raw fasta file 
 DNAStringMatrix = []
 
 with open(sys.argv[1]) as f:
-    DNAStringMatrix.append([line for line in f if not ">" in line])
-    
-print(DNAStringMatrix)
-        
+    for line in f.readlines():
+        if ">" in line:
+            continue # Skip ID lines 
+        else:
+            string= [] # Temp list to hold bases from single line
+            for char in line:
+                if char == "\n":
+                    continue # Don't include newline characters 
+                string.append(char) # Add bases to string list
+            DNAStringMatrix.append(string) # Append sequence to main list as a list 
+
+# Convert to numpy array 
+DNAStringArray = np.array(DNAStringMatrix)
+print(DNAStringArray)
+
